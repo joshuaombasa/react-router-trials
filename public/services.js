d192b74b6cb1687e4e0e5678fa0eeb058a0f091a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-16 23:19:40 +0300

    updated data

--- public/services.js
+++ public/services.js
@@ -1,25 +1,25 @@
 export const servicesArray = [
     {
         name: "combinator",
-        tools: ["motor, tester, level meter"],
+        tools: ["motor, tester, level-meter"],
         id: '0',
         price: 14
     },
     {
         name: "Plumbing",
-        ingredients: ["LN -key, Pipe range, washer"],
+        tools: ["LN-key, Pipe-range, washer"],
         price: 12,
         id: '1'
     },
         {
         name: "Wiring",
-        ingredients: ["J6 Cable, hops, ladder, mcbs"],
+        tools: ["J6-Cable, hops, ladder, mcbs"],
         price: 12,
         id: '2'
     },
     {
         name: "Attenuation",
-        ingredients: ["CAT6 cable, Filter Capacitor"],
+        tools: ["CAT6-cable, Filter-Capacitor"],
         price: 17,
         id: '3'
     }
