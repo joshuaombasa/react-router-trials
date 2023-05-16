81b4f28d7043683cb81b94a278f69769f33e5fde
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-16 20:54:12 +0300

    added media data

---
+++ public/services.js
@@ -0,0 +1,26 @@
+export const servicesArray = [
+    {
+        name: "combinator",
+        tools: ["motor, tester, level meter"],
+        id: '0',
+        price: 14
+    },
+    {
+        name: "Plumbing",
+        ingredients: ["LN -key, Pipe range, washer"],
+        price: 12,
+        id: '1'
+    },
+        {
+        name: "Wiring",
+        ingredients: ["J6 Cable, hops, ladder, mcbs"],
+        price: 12,
+        id: '2'
+    },
+    {
+        name: "Attenuation",
+        ingredients: ["CAT6 cable, Filter Capacitor"],
+        price: 17,
+        id: '3'
+    }
+]
