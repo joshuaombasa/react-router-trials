c0487ab154b4a72396666e4f790eca92e65ecda5
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-16 23:18:45 +0300

    updated jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,21 +1,20 @@
 import React  from "react";
-import { Link } from "react-router-dom";
+import { Link,Outlet } from "react-router-dom";
 import { servicesArray } from "../../public/services";
 
 
 export default function () {
-
-    
    const servicesJsx = servicesArray.map(item => {
-    return  <h1 key={item.id}><Link to={`/about/serices/${item.name}`}>{item.name}</Link></h1>
+    return  <h1 key={item.id}><Link to={`/about/serices/${item.id}`}>{item.name}</Link></h1>
    })
 
-   console.log(servicesJsx)
+  
 
     return (
         <div className="container">
             <h1>This is services page</h1>
             {servicesJsx}
+            <Outlet/>
         </div>
     )
 }
