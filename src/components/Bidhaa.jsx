349841f7a57b8d6f8ff85235e4b41b00bb057c6c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-16 23:18:14 +0300

    added jsx

---
+++ src/components/Bidhaa.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Link,useParams,useNavigate } from "react-router-dom";
+import { servicesArray } from "../../public/services";
+export default function Bidhaa(props) {
+
+    const navigate = useNavigate()
+
+    const params = useParams()
+     console.log(params.serviceId)
+
+     const selectedService = servicesArray.find(item => item.id === params.serviceId)
+     console.log(selectedService)
+
+    return (
+        <div className="service--container">
+            <h1 onClick={() => navigate(`/about/serices`)}
+            >service: {selectedService.name}</h1>
+            <p>Tools needed: {selectedService.tools}</p>
+            <p>price: ${selectedService.price}</p>
+        </div>
+    )
+}
