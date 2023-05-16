44975dc2333f2c0e54e6c41c41e4ef5b2bc18ea9
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-16 20:53:43 +0300

    added jsx

---
+++ src/assets/Service.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default function Service() {
+    return (
+        <div className="service--container">
+            <h1>This as a service</h1>
+        </div>
+    )
+}
