396869b1a5ed32945bfe46c16edee0f3e5ef62aa
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-22 22:21:27 +0300

    updated jsx

---
+++ src/pages/Events.jsx
@@ -0,0 +1,22 @@
+import React  from "react";
+
+export default function () {
+    return (
+        <div className="container">
+            <h1>This is events page</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Integer nec odio. Praesent
+                libero. Sed cursus ante dapibus diam. Sed
+                nisi. Nulla quis sem at nibh elementum
+                imperdiet. Duis sagittis ipsum. Praesent
+                mauris. Fusce nec tellus sed augue semper
+                porta. Mauris massa. Vestibulum lacinia arcu
+                eget nulla. Class aptent taciti sociosqu ad
+                litora torquent per conubia nostra, per
+                inceptos himenaeos. Curabitur sodales ligula
+                in libero.
+            </p>
+        </div>
+    )
+}
