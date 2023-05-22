91c00c4848cefdcd800509c9ee273392d5ccec8e
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-22 22:21:13 +0300

    updated jsx

---
+++ src/pages/FoodItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link,useParams } from 'react-router-dom'
+import { menuArray } from '../../public/data'
+
+export default function FoodItem(props) {
+    const params = useParams()
+   
+
+    const chosenItem = menuArray.find(item => item.id === params.productsId)
+    console.log(chosenItem)
+
+    return (
+        <div className="food-container">
+            <h2 className="name">{chosenItem.name} {chosenItem.emoji}</h2>
+            <p className="ingredients">{chosenItem.ingredients.join(' ')}</p>
+            <p className="price">$ {chosenItem.price}</p>
+        </div>
+    )
+}
